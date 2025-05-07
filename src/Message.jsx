1823c6b27b6b5595c2b183de01a0ec67890c8f94
2amkoding <2amkoding@gmail.com>
2025-05-07 18:04:32 -0500

    dynamic content

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,7 +1,11 @@
 //PascalCasing: jsx best practice
 function Message()  {
     // JSX
-    return <h1>Hello World</h1>;
+    // dynamic content
+    const name = 'June';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return  <h1>Hello  World</h1>;
 }
 
 export default Message;
