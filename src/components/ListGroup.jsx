d3163d70f1928e41fe70f826cf413e8bcfae152b
2amkoding <2amkoding@gmail.com>
2025-05-08 16:55:25 -0500

    managing state: aka useState

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = [
     "New York",
@@ -9,11 +11,20 @@ function ListGroup() {
 
  //items = [];
 
+ 
+ // Hook
+ const [selectedIndex, setSelectedIndex] = useState(-1);
+
+
+
   const sharedBlockStyles =
     "w-48 text-lg font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white";
 
 //Event Handler
-const handleClick = (event)=> console.log(event)
+//const handleClick = (event)=> console.log(event)
+const handleClick = (index) => {
+    setSelectedIndex(index);
+};
 
   return (
     <>
@@ -26,13 +37,15 @@ const handleClick = (event)=> console.log(event)
         <ul>
           {items.map((item, index) => (
             <li
-              className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600"
+              //className="w-full px-4 py-2 border-b border-gray-200 //dark:border-gray-600 hover:bg-sky-700 active:bg-violet-700"
               key={item}
               //onClick={()=> console.log("clicked")}
               //onClick={()=> console.log(item)}
               //onClick={()=> console.log(item,index)}
               //onClick={(event)=> console.log(event)}
-              onClick={handleClick}
+              //onClick={handleClick}
+              onClick={() => handleClick(index)}
+              className={`w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600 cursor-pointer ${index === selectedIndex ? "bg-blue-500 text-white" : "hover:bg-sky-700"}`}
             >
               {item}
             </li>
