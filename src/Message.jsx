463ed219b84a9d7607c8d607c7ded30c1a184f9a
2amkoding <2amkoding@gmail.com>
2025-05-08 11:32:00 -0500

    install tailwindCSS4.1

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,7 +4,7 @@ function Message()  {
     // dynamic content
     const name = 'June';
     if(name)
-        return <h1>Hello {name}</h1>;
+        return <h1 className="text-3xl font-bold underline">Hello {name}</h1>;
     return  <h1>Hello  World</h1>;
 }
 
